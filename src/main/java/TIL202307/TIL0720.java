8ce2f7e6be61f0d819df6816f7ad9622843ad44d
pc <qufekf97@gmail.com>
2023-07-23 23:02:54 +0900

    0723

--- src/main/java/TIL202307/TIL0720.java
+++ src/main/java/TIL202307/TIL0720.java
@@ -1,3 +1,5 @@
+package TIL202307;
+
 public class TIL0720{
     /* 리스트에서 toString을 오버라이드 하지 않으면 리스트 foreach 이후 sout 해도
        리스트 내용물이 나오지 않는다.
---
+++ src/main/java/TIL202307/TIL0723.java
@@ -0,0 +1,6 @@
+package TIL202307;
+
+public class TIL0723 {
+    /*  lombok 어노테이션 태그에서 toString 태그의 경우 필드에 객체가 있고 그 객체 내에 toString이 있으면
+        순환참조가 될 수 있기 때문에 주의해야한다.*/
+}
