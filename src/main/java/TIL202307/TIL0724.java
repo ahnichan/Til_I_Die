69c93cc0fb8d4b4b0d4bbe10b18ee6c30b40a392
pc <qufekf97@gmail.com>
2023-07-24 22:11:40 +0900

    0724

---
+++ src/main/java/TIL202307/TIL0724.java
@@ -0,0 +1,10 @@
+package TIL202307;
+
+public class TIL0724 {
+    /*  json 파일은 html과 연결할 수 있다.
+        stringify : object를 json 파일로 변환
+        parse : json파일을 다시 object로 변환
+        console.log() 자바에서의 sout과 비슷하게 페이지 내의 콘솔에 출력해준다.
+        함수의 경우 json에 포함되지 않기때문에 콘솔에 출력되지 않는다.
+        */
+}
