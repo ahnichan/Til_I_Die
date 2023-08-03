f8a7bc77c7bda347597423829b0f9b2e60cb282d
anichan <qufekf97@gmail.com>
2023-08-03 22:33:20 +0900

    0803

---
+++ src/main/java/TIL202308/TIL0803.java
@@ -0,0 +1,8 @@
+package TIL202308;
+
+public class TIL0803 {
+    /* 배열에서 .sort 를 통해 오름차순 혹은 내림차순으로 정렬 할 수 있다.
+    * function(left, right){ return right - left;} 의 경우는 내림차순
+    * return의 값을 반대로 하면 오름차순이 된다.
+    * right - left가 음수가 되면 순서를 뒤바꾸고 앞 순서로 돌아가는 식으로 순서를 맞춘다. */
+}
