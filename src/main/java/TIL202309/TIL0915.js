2fb4ded12f2b5bf1424bb74ea98d5cb84a7aa2f4
anichan <qufekf97@gmail.com>
2023-09-15 23:40:16 +0900

    20230915

---
+++ src/main/java/TIL202309/TIL0915.js
@@ -0,0 +1,78 @@
+<div className="Frame" style={{width: 460, height: 531, position: 'relative', background: 'white', boxShadow: '0px 5px 20px rgba(0, 0, 0, 0.15)', borderRadius: 8}}>
+    <div className="Frame" style={{width: 412, height: 790, left: 24, top: 33.50, position: 'absolute'}}>
+        <div className="Frame" style={{width: 340, height: 48, paddingTop: 15.50, paddingBottom: 16.50, paddingLeft: 113.42, paddingRight: 113.22, left: 36, top: 524.50, position: 'absolute', background: '#0F62FE', boxShadow: '0px 1px 2px rgba(0, 0, 0, 0.09)', borderRadius: 4, border: '1px #0F62FE solid', justifyContent: 'center', alignItems: 'center', display: 'inline-flex'}}>
+            <div style={{width: 113.36, height: 16, textAlign: 'center', color: 'white', fontSize: 14, fontFamily: 'Pretendard', fontWeight: '400', lineHeight: 15.40, wordWrap: 'break-word'}}>회원가입 완료</div>
+        </div>
+        <div className="Frame" style={{width: 412, height: 58, left: 0.37, top: 219.50, position: 'absolute'}}>
+            <div style={{width: 459, height: 114.50, left: -23.37, top: 163.50, position: 'absolute'}}>
+                <div className="Rectangle3" style={{width: 459, height: 114, left: 0, top: 0.50, position: 'absolute', background: 'white', borderTopLeftRadius: 4, borderTopRightRadius: 4}} />
+                <div className="Rectangle" style={{width: 412, height: 1, left: 23.37, top: 0, position: 'absolute', background: '#EFF1F4'}} />
+                <div style={{width: 412, height: 61, left: 23, top: 22.50, position: 'absolute', background: 'white'}}>
+                    <div style={{width: 412, height: 37, left: 0, top: 24, position: 'absolute'}}><span style="color: '#6B6F76', fontSize: 12, fontFamily: 'Pretendard', fontWeight: '400', textDecoration: 'underline', lineHeight: 20, wordWrap: 'break-word'">서비스 이용약관<br/></span><span style="color: '#6B6F76', fontSize: 12, fontFamily: 'Pretendard', fontWeight: '400', textDecoration: 'underline', lineHeight: 20, wordWrap: 'break-word'">개인정보처리방침</span></div>
+                    <div className="Frame2" style={{width: 262, height: 16, left: 0, top: 0, position: 'absolute', justifyContent: 'flex-start', alignItems: 'flex-start', gap: 10, display: 'inline-flex'}}>
+                        <div style={{width: 256, height: 16, color: '#282A30', fontSize: 14, fontFamily: 'Pretendard', fontWeight: '400', lineHeight: 15.40, wordWrap: 'break-word'}}>이용약관 모두 동의</div>
+                    </div>
+                    <div style={{width: 7, height: 32, left: 104, top: -13, position: 'absolute', color: '#FF3A3A', fontSize: 13, fontFamily: 'Inter', fontWeight: '500', wordWrap: 'break-word'}}>*</div>
+                </div>
+            </div>
+            <div className="CheckBox" style={{width: 14, height: 14, left: 114.63, top: 187.50, position: 'absolute'}}>
+                <div className="Rectangle2" style={{width: 14, height: 14, left: 0, top: 0, position: 'absolute', borderRadius: 2, border: '1px #A2A9B0 solid'}} />
+            </div>
+            <div className="Frame" style={{width: 412, height: 383, left: -0.37, top: 82.50, position: 'absolute'}}>
+                <div style={{left: 2, top: 6, position: 'absolute', color: '#343A3F', fontSize: 14, fontFamily: 'Pretendard', fontWeight: '400', lineHeight: 15.40, wordWrap: 'break-word'}}>직업</div>
+                <div style={{width: 7, height: 21, left: 26.63, top: 0.50, position: 'absolute', color: '#FF3A3A', fontSize: 13, fontFamily: 'Inter', fontWeight: '500', wordWrap: 'break-word'}}>*</div>
+                <div className="Component3" style={{paddingTop: 7, paddingBottom: 7, paddingLeft: 11, paddingRight: 207.08, left: 0, top: 29, position: 'absolute', background: 'white', boxShadow: '0px 0px 0px 1px #DFE1E4', borderRadius: 4, overflow: 'hidden', justifyContent: 'flex-start', alignItems: 'flex-start', display: 'inline-flex'}}>
+                    <div className="Frame" style={{width: 193.92, paddingRight: 93.92, justifyContent: 'flex-start', alignItems: 'center', display: 'flex'}}>
+                        <div className="AutoLayoutHorizontal" style={{width: 100, alignSelf: 'stretch', justifyContent: 'flex-start', alignItems: 'center', display: 'inline-flex'}}>
+                            <div style={{width: 100, color: '#878D96', fontSize: 12, fontFamily: 'Pretendard', fontWeight: '400', lineHeight: 18, wordWrap: 'break-word'}}>직업군을 골라주세요.</div>
+                        </div>
+                    </div>
+                    <div className="AutoLayoutHorizontal" style={{paddingTop: 11.50, paddingBottom: 11.50, justifyContent: 'flex-start', alignItems: 'flex-start', display: 'flex'}}>
+                        <div className="Frame" style={{width: 10, height: 5, justifyContent: 'center', alignItems: 'center', display: 'flex'}}>
+                            <div className="Frame" style={{width: 10, height: 5, position: 'relative', flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'flex-start', display: 'flex'}}>
+                                <div className="Vector" style={{width: 7.42, height: 4.27, background: '#4D5358'}}></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div style={{left: 2, top: 6, position: 'absolute', color: '#3C4149', fontSize: 14, fontFamily: 'Pretendard', fontWeight: '400', lineHeight: 15.40, wordWrap: 'break-word'}}>나이</div>
+            <div style={{width: 7, height: 21, left: 26.63, top: 0.50, position: 'absolute', color: '#FF3A3A', fontSize: 13, fontFamily: 'Inter', fontWeight: '500', wordWrap: 'break-word'}}>*</div>
+            <div className="Component1" style={{paddingTop: 7, paddingBottom: 7, paddingLeft: 11, paddingRight: 274.73, left: -0.37, top: 28.50, position: 'absolute', background: 'white', boxShadow: '0px 0px 0px 1px #DFE1E4', borderRadius: 4, overflow: 'hidden', justifyContent: 'flex-start', alignItems: 'flex-start', display: 'inline-flex'}}>
+                <div className="Frame" style={{paddingRight: 26.63, justifyContent: 'flex-start', alignItems: 'center', display: 'flex'}}>
+                    <div className="AutoLayoutHorizontal" style={{width: 100, alignSelf: 'stretch', justifyContent: 'flex-start', alignItems: 'center', display: 'inline-flex'}}>
+                        <div style={{color: '#878D96', fontSize: 12, fontFamily: 'Pretendard', fontWeight: '400', lineHeight: 18, wordWrap: 'break-word'}}>나이대를 골라주세요.</div>
+                    </div>
+                </div>
+                <div className="AutoLayoutHorizontal" style={{height: 28, paddingTop: 11.50, paddingBottom: 11.50, justifyContent: 'flex-start', alignItems: 'flex-start', display: 'flex'}}>
+                    <div className="Frame" style={{width: 10, height: 5, justifyContent: 'center', alignItems: 'center', display: 'flex'}}>
+                        <div className="Frame" style={{width: 10, height: 5, position: 'relative', flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'flex-start', display: 'flex'}}>
+                            <div className="Vector" style={{width: 7.42, height: 4.27, background: '#4D5358'}}></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div className="Frame" style={{width: 412, height: 58, left: 0.37, top: 138, position: 'absolute'}}>
+            <div style={{left: 2, top: 6, position: 'absolute', color: '#3C4149', fontSize: 14, fontFamily: 'Pretendard', fontWeight: '400', lineHeight: 15.40, wordWrap: 'break-word'}}>성별</div>
+            <div style={{width: 7, height: 21, left: 26.63, top: 0, position: 'absolute', color: '#FF3A3A', fontSize: 13, fontFamily: 'Inter', fontWeight: '500', wordWrap: 'break-word'}}>*</div>
+            <div className="LoginSex" style={{width: 412, paddingTop: 7, paddingBottom: 7, paddingLeft: 11, paddingRight: 274.73, left: -0.37, top: 29, position: 'absolute', background: 'white', boxShadow: '0px 0px 0px 1px #DFE1E4', borderRadius: 4, overflow: 'hidden', justifyContent: 'flex-start', alignItems: 'flex-start', display: 'inline-flex'}}>
+                <div className="AutoLayoutHorizontal" style={{width: 90, justifyContent: 'flex-start', alignItems: 'center', display: 'flex'}}>
+                    <div style={{width: 90, color: '#878D96', fontSize: 12, fontFamily: 'Pretendard', fontWeight: '400', lineHeight: 18, wordWrap: 'break-word'}}>성별을 골라주세요.</div>
+                </div>
+                <div className="AutoLayoutHorizontal" style={{height: 28, paddingTop: 11.50, paddingBottom: 11.50, justifyContent: 'flex-start', alignItems: 'flex-start', display: 'flex'}}>
+                    <div className="Frame" style={{width: 10, height: 5, justifyContent: 'center', alignItems: 'center', display: 'flex'}}>
+                        <div className="Frame" style={{width: 10, height: 5, position: 'relative', flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'flex-start', display: 'flex'}}>
+                            <div className="Vector" style={{width: 7.42, height: 4.27, background: '#4D5358'}}></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div className="Logo" style={{width: 121.55, height: 21, left: 145, top: -4, position: 'absolute', justifyContent: 'flex-end', alignItems: 'center', display: 'inline-flex'}}>
+            <div className="Heading" style={{width: 174.55, height: 21.38, color: '#5D5A88', fontSize: 17.49, fontFamily: 'DM Sans', fontWeight: '700', lineHeight: 20.61, wordWrap: 'break-word'}}>척추의 요정 로고</div>
+        </div>
+    </div>
+    <div style={{left: 139.37, top: 77, position: 'absolute', textAlign: 'center', color: '#282A30', fontSize: 24, fontFamily: 'Pretendard', fontWeight: '500', lineHeight: 32, wordWrap: 'break-word'}}>반가워요, ㅇㅇㅇ님!</div>
+    <div style={{width: 430.73, left: 15, top: 116, position: 'absolute', textAlign: 'center', color: '#6B6F76', fontSize: 15, fontFamily: 'Pretendard', fontWeight: '400', lineHeight: 23, wordWrap: 'break-word'}}>ㅇㅇㅇ님에 대해 좀 더 알려주세요.</div>
+</div>
