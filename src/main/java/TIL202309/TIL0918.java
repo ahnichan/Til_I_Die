dba78ea51e6fea56e57ccdfeba5c1aa1f65175dd
anichan <qufekf97@gmail.com>
2023-09-18 14:15:21 +0900

    20230918

---
+++ src/main/java/TIL202309/TIL0918.java
@@ -0,0 +1,7 @@
+package TIL202309;
+
+public class TIL0918 {
+    /* 자바스크립트에서 문자열에
+    * 문자열.padStart(목표 문자열 길이, 나머지 채울 것) 을 하면 문자열이 빈 곳에 채워 넣어 준다.
+    * padStart는 앞에 채워 넣고, padEnd는 뒤에 채워 넣는다. */
+}
