b72f09493f0ba35d0023187187fe2f9a51306d47
pc <qufekf97@gmail.com>
2023-10-02 17:06:38 +0900

    20231002

---
+++ src/main/java/TIL202310/TIL1002.js
@@ -0,0 +1,245 @@
+
+import * as posenet from '@tensorflow-models/posenet';
+import * as tf from '@tensorflow/tfjs-core';
+import dayjs from 'dayjs';
+import * as app from '../UserCam'
+
+const color = 'aqua';
+const boundingBoxColor = 'red';
+const lineWidth = 2;
+
+export const tryResNetButtonName = 'tryResNetButton';
+export const tryResNetButtonText = '[New] Try ResNet50';
+const tryResNetButtonTextCss = 'width:100%;text-decoration:underline;';
+const tryResNetButtonBackgroundCss = 'background:#e61d5f;';
+
+function isAndroid() {
+    return /Android/i.test(navigator.userAgent);
+}
+
+function isiOS() {
+    return /iPhone|iPad|iPod/i.test(navigator.userAgent);
+}
+
+export function isMobile() {
+    return isAndroid() || isiOS();
+}
+
+function setDatGuiPropertyCss(propertyText, liCssString, spanCssString = '') {
+    var spans = document.getElementsByClassName('property-name');
+    for (var i = 0; i < spans.length; i++) {
+        var text = spans[i].textContent || spans[i].innerText;
+        if (text == propertyText) {
+            spans[i].parentNode.parentNode.style = liCssString;
+            if (spanCssString !== '') {
+                spans[i].style = spanCssString;
+            }
+        }
+    }
+}
+
+export function updateTryResNetButtonDatGuiCss() {
+    setDatGuiPropertyCss(
+        tryResNetButtonText, tryResNetButtonBackgroundCss,
+        tryResNetButtonTextCss);
+}
+
+
+export function toggleLoadingUI(
+    showLoadingUI, loadingDivId = 'loading', mainDivId = 'main') {
+    if (showLoadingUI) {
+        document.getElementById(loadingDivId).style.display = 'block';
+        document.getElementById(mainDivId).style.display = 'none';
+    } else {
+        document.getElementById(loadingDivId).style.display = 'none';
+        document.getElementById(mainDivId).style.display = 'block';
+    }
+}
+
+function toTuple({y, x}) {
+    return [y, x];
+}
+
+export function drawPoint(ctx, y, x, r, color) {
+    ctx.beginPath();
+    ctx.arc(x, y, r, 0, 2 * Math.PI);
+    ctx.fillStyle = color;
+    ctx.fill();
+}
+
+
+export function drawSegment([ay, ax], [by, bx], color, scale, ctx) {
+    ctx.beginPath();
+    ctx.moveTo(ax * scale, ay * scale);
+    ctx.lineTo(bx * scale, by * scale);
+    ctx.lineWidth = lineWidth;
+    ctx.strokeStyle = color;
+    ctx.stroke();
+}
+
+
+export function drawSkeleton(keypoints, minConfidence, ctx, scale = 1) {
+    const adjacentKeyPoints =
+        posenet.getAdjacentKeyPoints(keypoints, minConfidence);
+
+    adjacentKeyPoints.forEach((keypoints) => {
+        drawSegment(
+            toTuple(keypoints[0].position), toTuple(keypoints[1].position), color,
+            scale, ctx);
+    });
+}
+
+
+export let last_normal_shoulder = '';
+export let last_wrong_shoulder = '';
+let last_normal_shoulder2 = '';
+let last_wrong_shoulder2 = '';
+let shoulder_start_date = '';
+export let shoulderTime = '';
+export let shoulderList = [];
+export let updateShoulderList = [];
+export const initParam = () => {
+    if(last_wrong_shoulder - last_normal_shoulder > 6000){
+        shoulderTime = shoulderTime + last_wrong_shoulder - last_normal_shoulder;
+        let shoulder = {
+            "posture_tag" : "ASYMMETRY",
+            "date" : shoulder_start_date,
+            "start_time" : last_normal_shoulder2,
+            "end_time" : last_wrong_shoulder2,
+        }
+        shoulderList.push(shoulder);
+    };
+}
+export const initPoseList = () => {
+    updateShoulderList = [];
+}
+
+
+export function drawKeypoints(keypoints, minConfidence, ctx, scale = 1) {
+    for (let i = 0; i < keypoints.length; i++) {
+        const keypoint = keypoints[i];
+
+        if (keypoint.score < minConfidence) {
+            continue;
+        }
+
+        const {y, x} = keypoint.position;
+        drawPoint(ctx, y * scale, x * scale, 3, color);
+    }
+    let nowTime = Math.floor(new Date().getTime());
+    let nowTimeFix = dayjs().format("HH:mm:ss");
+    let nowTimeFix2 = dayjs().format("YYYY-MM-DD");
+    if(keypoints[5] && keypoints[6] && app.firstFaceSize){
+
+        if(Math.abs(keypoints[5].position.y - keypoints[6].position.y) > 30){
+            last_wrong_shoulder = nowTime;
+            last_wrong_shoulder2 = nowTimeFix;
+            if(last_wrong_shoulder - last_normal_shoulder > 6000){
+                console.log("어깨가 이상해요");
+            }
+        }else{
+            if((last_wrong_shoulder - last_normal_shoulder > 6000) && last_normal_shoulder2){
+                shoulderTime = shoulderTime + last_wrong_shoulder - last_normal_shoulder;
+                let shoulder = {
+                    "posture_tag" : "ASYMMETRY",
+                    "date" : shoulder_start_date,
+                    "start_time" : last_normal_shoulder2,
+                    "end_time" : last_wrong_shoulder2,
+                }
+                shoulderList.push(shoulder);
+                updateShoulderList.push(shoulder);
+            }
+            last_normal_shoulder = nowTime;
+            last_normal_shoulder2 = nowTimeFix;
+            shoulder_start_date = nowTimeFix2;
+
+        }
+    }
+}
+
+
+export function drawBoundingBox(keypoints, ctx) {
+    const boundingBox = posenet.getBoundingBox(keypoints);
+
+    ctx.rect(
+        boundingBox.minX, boundingBox.minY, boundingBox.maxX - boundingBox.minX,
+        boundingBox.maxY - boundingBox.minY);
+
+    ctx.strokeStyle = boundingBoxColor;
+    ctx.stroke();
+}
+
+
+export async function renderToCanvas(a, ctx) {
+    const [height, width] = a.shape;
+    const imageData = new ImageData(width, height);
+
+    const data = await a.data();
+
+    for (let i = 0; i < height * width; ++i) {
+        const j = i * 4;
+        const k = i * 3;
+
+        imageData.data[j + 0] = data[k + 0];
+        imageData.data[j + 1] = data[k + 1];
+        imageData.data[j + 2] = data[k + 2];
+        imageData.data[j + 3] = 255;
+    }
+
+    ctx.putImageData(imageData, 0, 0);
+}
+
+
+export function renderImageToCanvas(image, size, canvas) {
+    canvas.width = size[0];
+    canvas.height = size[1];
+    const ctx = canvas.getContext('2d');
+
+    ctx.drawImage(image, 0, 0);
+}
+
+
+export function drawHeatMapValues(heatMapValues, outputStride, canvas) {
+    const ctx = canvas.getContext('2d');
+    const radius = 5;
+    const scaledValues = heatMapValues.mul(tf.scalar(outputStride, 'int32'));
+
+    drawPoints(ctx, scaledValues, radius, color);
+}
+
+
+function drawPoints(ctx, points, radius, color) {
+    const data = points.buffer().values;
+
+    for (let i = 0; i < data.length; i += 2) {
+        const pointY = data[i];
+        const pointX = data[i + 1];
+
+        if (pointX !== 0 && pointY !== 0) {
+            ctx.beginPath();
+            ctx.arc(pointX, pointY, radius, 0, 2 * Math.PI);
+            ctx.fillStyle = color;
+            ctx.fill();
+        }
+    }
+}
+
+
+// export function drawOffsetVectors(
+//     heatMapValues, offsets, outputStride, scale = 1, ctx) {
+//   const offsetPoints =
+//       posenet.singlePose.getOffsetPoints(heatMapValues, outputStride, offsets);
+
+//   const heatmapData = heatMapValues.buffer().values;
+//   const offsetPointsData = offsetPoints.buffer().values;
+
+//   for (let i = 0; i < heatmapData.length; i += 2) {
+//     const heatmapY = heatmapData[i] * outputStride;
+//     const heatmapX = heatmapData[i + 1] * outputStride;
+//     const offsetPointY = offsetPointsData[i];
+//     const offsetPointX = offsetPointsData[i + 1];
+
+//     drawSegment(
+//         [heatmapY, heatmapX], [offsetPointY, offsetPointX], color, scale, ctx);
+//   }
+// }
