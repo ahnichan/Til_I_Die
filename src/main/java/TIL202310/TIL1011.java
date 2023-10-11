4d138f8de637be2f5fbd5d7eb65d33f713e05605
anichan <qufekf97@gmail.com>
2023-10-11 16:26:01 +0900

    20231011

---
+++ src/main/java/TIL202310/TIL1011.java
@@ -0,0 +1,12 @@
+package TIL202310;
+
+public class TIL1011 {
+    /* react 환경에서 생명주기 관련 메소드들
+    * constructor : 컴포넌트를 새로 만들 때 마다 호출되는 클래스 생성자 메소드
+    * getDerivedStateFromProps : props에 있는 값을 state에 넣을 때 사용하는 메소드
+    * componentDidMount : 컴포넌트가 웹 브라우저 상에 나타난 후 호출되는 메소드
+    * shouldComponentUpdate : 컴포넌트가 리렌더링을 할 것인지 말 것인지를 결정하는 메소드
+    * getSnapshotBeforeUpdate : 컴포넌트의 변화를 실제 DOM에 반영하기 직전에 호출하는 메소드
+    * componentDidUpdate : 컴포넌트 업데이트 작업이 끝난 후 호출하는 메소드
+    * componentWillUnmount : 컴포넌트가 웹 브라우저 상에서 사라지기 직전에 호출하는 메소드*/
+}
