53256d2ebd0fed894aabeb60da3f3b5aa103ee7b
anichan <qufekf97@gmail.com>
2023-10-12 19:43:58 +0900

    20231012

---
+++ src/main/java/TIL202310/TIL1012.java
@@ -0,0 +1,9 @@
+package TIL202310;
+
+public class TIL1012 {
+
+    /*
+        리듀서를 사용하기 위한 기본형
+    const [해당 리듀서가 관리하고 반환할 state, 해당 리듀서 함수를 호출하기 위한 dispatch함수]
+            = useReducer(리듀서 함수, state 초기 값);*/
+}
