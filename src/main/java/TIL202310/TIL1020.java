77b1a0c8296c82875157648b55a57c805a83b470
anichan <qufekf97@gmail.com>
2023-10-20 17:17:55 +0900

    20231020

---
+++ src/main/java/TIL202310/TIL1020.java
@@ -0,0 +1,8 @@
+package TIL202310;
+
+public class TIL1020 {
+    /*
+    *  logger를 사용하여 action 객체의 모습(type 및 payload)과 state의 변화를
+    * console에 출력해 주어 action과 state의 변화를 log로 찍어준다.
+    * logger 미들웨어는 미들웨어 중 가장 늦게 실행되게 세팅된다. */
+}
