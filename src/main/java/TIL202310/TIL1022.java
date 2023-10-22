1555247a324f951117f0dd56998ff6a44bb6daec
pc <qufekf97@gmail.com>
2023-10-22 21:23:57 +0900

    20231022

---
+++ src/main/java/TIL202310/TIL1022.java
@@ -0,0 +1,7 @@
+package TIL202310;
+
+public class TIL1022 {
+    /* payload 객체를 반환하는 과정에서 비동기 통신을 하게 되면 비동기 처리가 끝난 값이 payload에 담기기 전에
+    * return 이 실행되어 promise 객체를 반환시키는 문제가 있다.
+    * 이는 미들웨어를 사용하여 해결 할 수 있다.*/
+}
