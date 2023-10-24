46df7aeb708d7617be9572f0a034235e0a1adcee
anichan <qufekf97@gmail.com>
2023-10-24 16:06:10 +0900

    20231024

---
+++ src/main/java/TIL202310/TIL1024.java
@@ -0,0 +1,7 @@
+package TIL202310;
+
+public class TIL1024 {
+    /* createActions에서 async await 함수를 사용하며 return문을 작성하면 값이 담기는 과정을 기다리지 않고
+    * return문이 실행되는 문제점이 있다.
+    * 따라서 미들웨어를 통해 return문을 작성하지 않고 await 함수를 실행하여 해결할 수 있다.*/
+}
