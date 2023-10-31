ed06b32eba3b2b4ecfbdfe13d128ae7be05c2dbc
anichan <qufekf97@gmail.com>
2023-10-31 20:02:10 +0900

    20231031

---
+++ src/main/java/TIL202310/TIL1031.java
@@ -0,0 +1,9 @@
+package TIL202310;
+
+public class TIL1031 {
+    /*
+    * 미들웨어를 구성하는 인자들을 확인해보면,
+    * store는 dispatch와 getstate 함수를 포함한 객체이고,
+    * next는 그 자체로 함수이며,
+    * action은 말 그대로 reducer에 전달되는 액션으로 구성되어 있다.*/
+}
