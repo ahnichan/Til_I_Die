14b605db31d9c81fbb05eccc0d4c89440553d972
pc <qufekf97@gmail.com>
2023-11-04 22:01:40 +0900

    20231104

---
+++ src/main/java/TIL202311/TIL1104.java
@@ -0,0 +1,6 @@
+package TIL202311;
+
+public class TIL1104 {
+    /* html 에서 script 태그로 "https://cdn.babylonjs.com/viewer/babylon.viewer.js"를 추가하면
+    * <babylon> 태그를 사용할 수 있는데 model = "모델 경로"를 넣으면 모델을 불러올 수 있다.*/
+}
