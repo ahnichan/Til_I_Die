e4c1f5c9b953e3761a75d27b6c7a938eaccbc559
anichan <qufekf97@gmail.com>
2023-12-05 15:32:34 +0900

    20231203수정

--- src/main/java/TIL202312/TIL1203.java
+++ src/main/java/TIL202312/TIL1203.java
@@ -2,5 +2,5 @@ package TIL202312;
 
 public class TIL1203 {
     /* 리액트 router 에서 path: 를 통해서 주소값을 설정 할 수 있고,
-    * element: 를 통해서 어떤 함수를 렌더링 할 것인지 설정할 수 있다.*/
+    * element: 를 통해서 어떤 컴포넌트를 렌더링 할 것인지 설정할 수 있다.*/
 }
