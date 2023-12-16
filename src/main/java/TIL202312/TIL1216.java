f0a91030cb0711952e9040a06c1ea642256d8918
pc <qufekf97@gmail.com>
2023-12-16 21:47:38 +0900

    20231216

---
+++ src/main/java/TIL202312/TIL1216.java
@@ -0,0 +1,6 @@
+package TIL202312;
+
+public class TIL1216 {
+    /* react router에서 useNavigation을 이용하여
+    * navigation.location를 사용하는 경우, 이것은 앱이 새 URL로 이동하여 해당 데이터를 로드할 때 표시되고 더 이상의 탐색이 없으면 사라진다.*/
+}
