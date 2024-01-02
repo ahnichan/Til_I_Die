fd7b70f04f8656fb091c6d52c479423c37d17acc
pc <qufekf97@gmail.com>
2024-01-02 21:01:23 +0900

    20230102

---
+++ src/main/java/TIL202401/TIL0102.java
@@ -0,0 +1,7 @@
+package TIL202401;
+
+public class TIL0102 {
+    /* NativeRouter는 React Native 앱을 만들 때 추천되는 인터페이스이다.
+    * <NativeRouter initialEntries>는 기본값으로 '/'를 사용한다.
+    * <NativeRouter initialIndex>는 기본값으로 initialEntries의 마지막 index를 사용한다.*/
+}
