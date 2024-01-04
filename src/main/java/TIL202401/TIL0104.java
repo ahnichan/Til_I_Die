72357949e4e3ec9c493e219c8038ca46b11cf831
pc <qufekf97@gmail.com>
2024-01-04 22:17:43 +0900

    20230104

---
+++ src/main/java/TIL202401/TIL0104.java
@@ -0,0 +1,7 @@
+package TIL202401;
+
+public class TIL0104 {
+    /* hydrateFallbackElement / 서버사이드렌더링을 사용하고 partial hydration을 활용하는 경우 non-hydrated routes를 위해
+     초기 hydration 중 렌더링할 element/component를 지정할 수 있다.
+    * */
+}
