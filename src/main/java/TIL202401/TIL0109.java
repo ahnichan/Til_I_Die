25b2dbfd4b65394e72f0b240b6af90fd5736c845
pc <qufekf97@gmail.com>
2024-01-10 18:58:45 +0900

    20230109수정

--- src/main/java/TIL202401/TIL0109.java
+++ src/main/java/TIL202401/TIL0109.java
@@ -1,7 +1,7 @@
 package TIL202401;
 
 public class TIL0109 {
-    /* data APIs fmf wldnjsgksms router는
+    /* data APIs 를 지원하는 router는
     * createBrowserRouter, createMemoryRouter, createHashRouter, createStaticRouter 등이 있다.
     * BrowserRouter, MemoryRouter, HashRouter, NativeRouter, StaticRouter 등은 지원하지 않는다.
     * NativeRouter의 경우는 현재 지원하지는 않지만 추후 지원될 것이라고 한다. */
