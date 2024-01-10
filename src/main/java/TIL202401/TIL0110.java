22a4d94233a834d56b47841a219b183877e4b939
pc <qufekf97@gmail.com>
2024-01-10 20:05:52 +0900

    20230110

---
+++ src/main/java/TIL202401/TIL0110.java
@@ -0,0 +1,7 @@
+package TIL202401;
+
+public class TIL0110 {
+    /* loader는 data router를 쓰는 경우에만 사용 가능하다.
+    *  앱을 탐색하는 동안, 다음으로 맞는 브랜치의 route의 loader들이 병렬로 호출된다.
+    * 그리고 그들의 데이터는 useLoaderData를 통해서 컴포넌트에 제공된다.*/
+}
