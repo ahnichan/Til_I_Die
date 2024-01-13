1e95f1e7f284ecb2691795f890fda4fb53b874b0
pc <qufekf97@gmail.com>
2024-01-13 14:28:59 +0900

    20230113

---
+++ src/main/java/TIL202401/TIL0113.java
@@ -0,0 +1,7 @@
+package TIL202401;
+
+public class TIL0113 {
+    /* loader에서 서버사이드렌더링을 수행하고 Partial Hydration을 위해 fututre.v7_partialHydration 플래그를 활용하는 경우,
+    * 초기 hydration시 route loader 실행을 선택할 수 있다.
+    * 이때 initial hydration에서 hydration data가 있더라도 route loader를 강제로 실행하도록 하려면 loader 함수에 hydrate property를 설정할 수 있다. */
+}
