922931a80b58d335f77ef46cc13ba592eab5b070
pc <qufekf97@gmail.com>
2024-01-14 22:54:03 +0900

    20230114

---
+++ src/main/java/TIL202401/TIL0114.java
@@ -0,0 +1,8 @@
+package TIL202401;
+
+public class TIL0114 {
+    /* 로더에서는 반환할 수 있는 값이 자유롭게 결정될 수 있으며, useLoaderData를 통해 접근할 수 있다.
+    * 또한 web Response를 반환 할 수도 있다.
+    *
+    * fetch의 반환값은 Response이기 때문에 많은 로더가 간단한 fetch를 반환할 수 있다. */
+}
