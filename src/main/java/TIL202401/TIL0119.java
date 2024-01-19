b5f5ea3e5d3dc537deed8861698bca1e3672016e
pc <qufekf97@gmail.com>
2024-01-19 23:26:18 +0900

    20230119

---
+++ src/main/java/TIL202401/TIL0119.java
@@ -0,0 +1,8 @@
+package TIL202401;
+
+public class TIL0119 {
+    /* <Await>에서 children은 react element일 수도 있고 함수일 수도 있다.
+    * 함수를 쓸 때, value는 매개변수로만 제공된다.
+    * react element를 사용할 때, useAsyncValue가 데이터를 제공한다.
+    * */
+}
