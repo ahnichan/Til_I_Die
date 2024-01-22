d6ff42f96f75242d243f7d52ba23c4267a4b13bd
pc <qufekf97@gmail.com>
2024-01-22 23:04:54 +0900

    20230122

---
+++ src/main/java/TIL202401/TIL0122.java
@@ -0,0 +1,7 @@
+package TIL202401;
+
+public class TIL0122 {
+    /* <Form> 내의 요소들은 렌더링된 useNavigation 훅에 state update을 일으켜서
+    비동기 작업이 진행 중일 때 보류중인 indicators 나 optimistic UI를 빌드할 수 있게 한다.
+    폼이 navigation 처럼 느껴지지 않는다면 useFetcher를 사용하는 것이 낫다.*/
+}
