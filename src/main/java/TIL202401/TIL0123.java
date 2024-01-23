966044d71303ab71118847aadd9d009fa2b8372c
pc <qufekf97@gmail.com>
2024-01-23 21:45:02 +0900

    20230123

---
+++ src/main/java/TIL202401/TIL0123.java
@@ -0,0 +1,6 @@
+package TIL202401;
+
+public class TIL0123 {
+    /* react router의 Form은 HTML form의 action과 유사하다. 유일한 차이점은 기본 action이다.
+    * HTML form의 경우 전체 URL이 기본이지만, react router의 form의 경우는 context 내에서 가장 가까운 route의 상대 URL로 설정된다.*/
+}
