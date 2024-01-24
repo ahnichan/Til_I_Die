08592cfee85ae8fafac40a4226dad4ad6993c907
pc <qufekf97@gmail.com>
2024-01-24 23:24:24 +0900

    20230124

---
+++ src/main/java/TIL202401/TIL0124.java
@@ -0,0 +1,6 @@
+package TIL202401;
+
+public class TIL0124 {
+    /* react router의 method는 사용될 HTTP verb를 결정한다.
+    * 일반적인 HTML form 메소드와 동일하지만 get과 post 이외에; 추가로 put, patch, delete를 지원한다. 기본값은 get이다.*/
+}
