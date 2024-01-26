e67d7fb4926a73bc0d4c473344162e12902d110e
pc <qufekf97@gmail.com>
2024-01-26 23:45:39 +0900

    20230126

---
+++ src/main/java/TIL202401/TIL0126.java
@@ -0,0 +1,7 @@
+package TIL202401;
+
+public class TIL0126 {
+    /* react router에서의 GET Submissions는
+    * 기본 메서드는 get이고, get으로 제출된 내용은 어떤 action도 호출하지 않는다.
+    * get으로 제출된 내용은 일반적인 navigation과 같으며, 차이점은 사용자가 form에서 URL로 전달되는 검색 매개변수를 제공할 수 있다는 것이다.*/
+}
