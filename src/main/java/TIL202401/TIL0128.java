c39cef69da1b84293dbeb272f4f09ed74d4ad0c7
pc <qufekf97@gmail.com>
2024-01-28 22:45:21 +0900

    20230128

---
+++ src/main/java/TIL202401/TIL0128.java
@@ -0,0 +1,7 @@
+package TIL202401;
+
+public class TIL0128 {
+    /* navigation을 건너뛰도록 form에 명령하고 내부적으로 fetch를 사용하도록 <Form navigate={false}> 를 사용할 수 있다.
+    * 이것은 본질적으로 useFetcher() + <fetcher.Form>의 단축키로, 결과 데이터에 대해 신경쓰지 않고 제출을 시작하고,
+    * useFetchers()를 통해 보류 중인 상태에 액세스하려는 경우에 사용된다.  */
+}
