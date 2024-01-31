c285ae228e3deda2807457d99ad464a719263939
pc <qufekf97@gmail.com>
2024-01-31 18:54:52 +0900

    20230131

---
+++ src/main/java/TIL202401/TIL0131.java
@@ -0,0 +1,7 @@
+package TIL202401;
+
+public class TIL0131 {
+    /* 기본적으로, path는 route 계층에 상대적이므로, ..은 한 단계 위의 route 레벨로 이동한다.
+    * 경우에 따라서 중첩이 의미없는 일치 URL 패턴이 있을 수 있고, 상대 경로 routing이 더 나은 경우가 있을 수 있다.
+    * 이 동작을 선택하려면 <Form to ="../some/where" relative="path"> 와 같이 사용할 수 있다. */
+}
