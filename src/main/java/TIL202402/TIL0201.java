5477af16aaeedba28566ce0260b7c3536994b0e6
pc <qufekf97@gmail.com>
2024-02-01 23:40:05 +0900

    20230201

---
+++ src/main/java/TIL202402/TIL0201.java
@@ -0,0 +1,7 @@
+package TIL202402;
+
+public class TIL0201 {
+    /* reloadDocument는 폼에 react router를 건너뛰고 브라우저의 기본 동작을 사용하여 폼을 제출하도록 한다.
+    * 이것은 기본 및 상대적 action의 이점을 얻기 위해 <form> 대신 권장되지만, 그 외에는 일반 HTML form과 동일하다.
+    * remix와 같은 프레임워크가 없거나 자체적으로 route에 대한 posts를 처리하지 않는 한, 이것은 유용하진 않다.*/
+}
