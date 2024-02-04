1c2734b8f9176d42078cdd59fdb9ed55f706a490
pc <qufekf97@gmail.com>
2024-02-04 23:42:44 +0900

    20230204

---
+++ src/main/java/TIL202402/TIL0204.java
@@ -0,0 +1,7 @@
+package TIL202402;
+
+public class TIL0204 {
+    /* unstable_viewTransition 속성은 최종 state update를 document.startViewTransition()으로 wrapping하여
+    * 이 navigation에 대한 view transition을 활성화한다.
+    * 이 view transition에 대한 특정 스타일을 적용해야하는 경우, unstable_useViewTransitionState()를 활용해야한다. */
+}
