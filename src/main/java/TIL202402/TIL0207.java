f39e5c772c29d764714946095e4958a26b6b3aa1
pc <qufekf97@gmail.com>
2024-02-07 23:37:22 +0900

    20230207

---
+++ src/main/java/TIL202402/TIL0207.java
@@ -0,0 +1,8 @@
+package TIL202402;
+
+public class TIL0207 {
+    /* 기본적으로 링크는 route 계층 구조를 기준으로 상대적이다.
+    * 즉 ..은 현재 context route에서 한 단계 위의 route 단계로 이동한다.
+    * 때로는 중첩이 의미없는 중복 URL 패턴이 있을 수 있으며, 현재 context route path에서 상대 경로 routing을
+    * 사용하려면 relative="path"로 동작할 수 있다. */
+}
