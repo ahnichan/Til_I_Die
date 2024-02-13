9d03bba7b8ce7f92c13c3f28422b430921358941
pc <qufekf97@gmail.com>
2024-02-13 23:09:51 +0900

    20230213

---
+++ src/main/java/TIL202402/TIL0213.java
@@ -0,0 +1,10 @@
+package TIL202402;
+
+public class TIL0213 {
+    /* unstable_viewTransition prop은 navigation을 위한 최종 state update를 document.startViewTransition()으로
+    * 래핑함으로써 view transition을 활성화 한다.
+    * <Link to={to} unstable_viewTransition>
+          Click me
+      </Link>
+    * */
+}
