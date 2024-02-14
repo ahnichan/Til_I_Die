fcb61eb9b019bd8dfc9b59bf072474c1958458f0
pc <qufekf97@gmail.com>
2024-02-14 23:35:47 +0900

    20230214

---
+++ src/main/java/TIL202402/TIL0214.java
@@ -0,0 +1,6 @@
+package TIL202402;
+
+public class TIL0214 {
+    /* 만약 이 view transition에 특정 스타일을 적용해야한다면, unstable_useViewTransitionState() 훅을 활용하여야 한다.
+    * (또는 NavLink에서 transitioning class와 isTransitioning render prop을 확인해야한다.) */
+}
