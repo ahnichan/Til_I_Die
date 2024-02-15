6ad189ed1fcc9439cac49bff8b9e24c35e222e3d
pc <qufekf97@gmail.com>
2024-02-15 23:01:11 +0900

    20230215

---
+++ src/main/java/TIL202402/TIL0215.java
@@ -0,0 +1,9 @@
+package TIL202402;
+
+public class TIL0215 {
+    /* <NavLink> 는 자신이 active상태인지, pending상태인지, transitioning상태인지의 여부를 알 수 있는 특별한 <Link>이다.
+    * 이것은 몇 가지의 시나리오에서 유용하다.
+    * 1. navigation 메뉴를 구축할 때, breadcrumb 혹은 현재 선택된 것을 표시하고 싶은 tabs 집합
+    * 2. 스크린 readers 와 같은 보조 기술에 유용한 context 제공
+    * 3. view transition에 대한 미세한 제어를 위해서 transitioning 값 제공 */
+}
