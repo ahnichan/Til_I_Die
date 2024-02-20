6395af259eaab7d6522eaf84cae17b1660e39411
pc <qufekf97@gmail.com>
2024-02-20 23:10:16 +0900

    20230220

---
+++ src/main/java/TIL202402/TIL0220.java
@@ -0,0 +1,6 @@
+package TIL202402;
+
+public class TIL0220 {
+    /* end prop은 활성 및 보류중인 state의 matching되는 logic을 변경하여 NavLink의 to 경로의 "끝"에만 일치하도록 한다.
+    * URL이 to보다 긴 경우 더 이상 active로 간주되지 않는다. */
+}
