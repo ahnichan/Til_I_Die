3cd409a39f1670443aa30f5c48570ada6a87c9bf
pc <qufekf97@gmail.com>
2024-02-21 22:37:45 +0900

    20230221

---
+++ src/main/java/TIL202402/TIL0221.java
@@ -0,0 +1,7 @@
+package TIL202402;
+
+public class TIL0221 {
+    /* root route로의 Link에 대한 참고사항
+    * <NavLink to ="/">는 모든 URL이 /에 일치하는 특수한 경우이다. 기본적으로 모든 route에 일치하는 것을 막기 위해,
+    * 이것은 사실상 end 속성을 무시하고 root route에 있을 때에만 일치한다. */
+}
