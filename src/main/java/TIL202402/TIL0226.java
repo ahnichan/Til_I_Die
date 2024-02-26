9a807e3234aa9b78c8af7e5479d9a71c4701e2d7
pc <qufekf97@gmail.com>
2024-02-26 22:14:10 +0900

    20230226

---
+++ src/main/java/TIL202402/TIL0226.java
@@ -0,0 +1,7 @@
+package TIL202402;
+
+public class TIL0226 {
+    /* unstable_viewTransition prop은 최종 state 업데이트를 document.startViewTransition()으로 감싸서
+    * 이 navigation에 대한 view transition을 활성화한다. 기본적으로 transition 중에는 <a> 요소에 transitioning
+    * 클래스가 추가되어 view transition을 커스터마이징 하는 데에 사용할 수 있다. */
+}
