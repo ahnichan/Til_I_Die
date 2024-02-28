d1c89d1ba03ddc96e27a2fe7b4c383a8370de3bd
pc <qufekf97@gmail.com>
2024-02-28 23:00:10 +0900

    20230228

---
+++ src/main/java/TIL202402/TIL0228.java
@@ -0,0 +1,7 @@
+package TIL202402;
+
+public class TIL0228 {
+    /* <Outlet>은 부모 route 요소에서 사용되어 자식 route 요소를 렌더링한다. 이는 자식 route가 렌더링 될 때,
+    * 중첩된 UI가 표시되도록 한다. 부모 route가 정확하게 일치하는 경우, 자식 index route를 렌더링하거나 index route가
+    * 없는 경우 아무것도 렌더링하지 않는다. */
+}
